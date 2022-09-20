7ed9eb57643a067455d49dae342600ea8fd55fb5
JaviLomark <lopezlarafranciscojavier@gmail.com>
2022-09-20 10:32:04 +0000

    Primer commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,33 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#excusas").innerHTML = excusas();
+  });
 };
+
+function excusas() {
+  let who = ["Un perro", "Un gato", "Una tortuga", "Un pajaro"];
+  let what = [
+    "se comio mi bocadillo",
+    "me tiro al suelo",
+    "me aplasto",
+    "me robo el coche"
+  ];
+  let when = [
+    "antes de clase",
+    "en el gimnasio",
+    "en la calle",
+    "en el trabajo",
+    "en casa"
+  ];
+
+  return (
+    who[Math.floor(Math.random() * who.length)] +
+    " " +
+    what[Math.floor(Math.random() * what.length)] +
+    " " +
+    when[Math.floor(Math.random() * when.length)]
+  );
+}
